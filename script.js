e27562c2e12a47db53a054d2df40263dc905b2d9
andresr1122 <andresr1122@hotmail.com>
2024-07-30 13:35:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('confirmButton').addEventListener('click', function() {
+    alert('¡Sé que dirás que sí! Nos vemos el jueves a las 8:30 PM ♥ (dale en aceptar aquí abajito)');
+});
